efe6a16dd388444dcc01101c606285ba57153abf
aswin <aswinkv95@gmail.com>
2026-03-03 22:39:43 +0530

    Updated reports components

--- My-App-1/my-app-l1-front/src/Reports/components/FileUpload.jsx
+++ My-App-1/my-app-l1-front/src/Reports/components/FileUpload.jsx
@@ -12,6 +12,7 @@ export default function FileUpload() {
     "RUPAY_NETWORK",
   ];
 
+  
   const handleFileChange = (e) => {
     setError("");
     setSuccess("");
--- My-App-1/my-app-l1-front/src/Reports/components/VisaException.jsx
+++ My-App-1/my-app-l1-front/src/Reports/components/VisaException.jsx
@@ -154,7 +154,6 @@ export default function VisaException() {
         </table>
       )}
 
-     
     </div>
   );
 }
